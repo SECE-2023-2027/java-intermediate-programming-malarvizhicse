ed3e2c267ea26e46906e59f84f93fa857730f2c9
MALARVIZHI S <malarvizhi.s2023cse@sece.ac.in>
2026-07-01 12:13:47 +0530

    Added Question 1 solution

---
+++ Question_01_Product/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Product ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Enter Product Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Price: ");
+        double price = sc.nextDouble();
+
+        System.out.print("Enter Stock Quantity: ");
+        int stock = sc.nextInt();
+
+        System.out.print("Enter Discount Percentage: ");
+        double discount = sc.nextDouble();
+
+        Product p = new Product(id, name, price, stock);
+
+        p.applyDiscount(discount);
+
+        System.out.println("Price after Discount : " + p.getPrice());
+        System.out.println("Stock Quantity : " + p.getStockQuantity());
+
+        sc.close();
+    }
+}
